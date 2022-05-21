011f2d0051d5fd614e2497e0f928d09d34e850fa
iykuetboo <iykuetboo@gmail.com>
2022-05-21 16:22:23 +0900

    flex textarea

---
+++ public/js/resize_textarea.js
@@ -0,0 +1,9 @@
+function flexTextarea(el) {
+    const dummy = el.querySelector('.FlexTextarea__dummy')
+    el.querySelector('.FlexTextarea__textarea').addEventListener('input', e => {
+      dummy.textContent = e.target.value + '\u200b'
+    })
+  }
+  
+  document.querySelectorAll('.FlexTextarea').forEach(flexTextarea)
+  
